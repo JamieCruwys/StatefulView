a77514639362f47a5a284a7a4c58ae7693b0253c
Jamie Cruwys <me@jamiecruwys.co.uk>
2017-04-03 00:23:30 +0100

    Added example main layout, but missing other layouts
    Signed-off-by: Jamie Cruwys <me@jamiecruwys.co.uk>

--- example/src/main/java/com/example/statefulview/example/MainActivity.java
+++ example/src/main/java/com/example/statefulview/example/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.statefulview.example;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.statefulview.State;
+import com.example.statefulview.StatefulView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity
 {
+    @BindView(R.id.stateful_view)
+    StatefulView statefulView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.contentButton) void onShowingContentState()
+    {
+        statefulView.setState(State.SHOWING_CONTENT);
+    }
+
+    @OnClick(R.id.emptyButton) void onEmptyState()
+    {
+        statefulView.setState(State.EMPTY);
+    }
+
+    @OnClick(R.id.loadingButton) void onLoadingState()
+    {
+        statefulView.setState(State.LOADING);
+    }
+
+    @OnClick(R.id.errorButton) void onErrorState()
+    {
+        statefulView.setState(State.ERROR);
+    }
+
+    @OnClick(R.id.offlineButton) void onOfflineState()
+    {
+        statefulView.setState(State.OFFLINE);
     }
 }
