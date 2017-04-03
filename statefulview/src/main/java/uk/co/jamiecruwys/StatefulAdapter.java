986889793377ce2eadfa074316de82dba03fbf7c
Jamie Cruwys <me@jamiecruwys.co.uk>
2017-04-03 12:41:19 +0100

    Allow for passing of the view holder

--- statefulview/src/main/java/uk/co/jamiecruwys/StatefulAdapter.java
+++ statefulview/src/main/java/uk/co/jamiecruwys/StatefulAdapter.java
@@ -2,11 +2,12 @@ package uk.co.jamiecruwys;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 
 /**
  * Created by Jamie Cruwys of 3 SIDED CUBE on 03/04/2017.
  */
-public abstract class StatefulAdapter extends RecyclerView.Adapter
+public abstract class StatefulAdapter<VH extends ViewHolder> extends RecyclerView.Adapter<VH>
 {
 	private AdapterStateListener stateListener;
 
